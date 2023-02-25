0157f20262510cb141c1c041ff4c228417d1bbc0
itate <itate.eg5@gmail.com>
2023-02-25 19:36:33 +0300

    Домашнее задание 4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lesson4;
+
+var generic = new SimpleGenericCache<string>();
+
+generic.Store("1","КЭШ");
+
+Console.WriteLine (generic.Fetch("1"));
+
+generic.Store("1", "КЭШ", 0); // Выход в ветку else, возвращает значение по умолчанию (пустоту)
+
+Console.WriteLine(generic.Fetch("1"));
---
+++ Lesson4/SimpleGenericCache.cs
@@ -0,0 +1,47 @@
+﻿namespace Lesson4
+{
+    internal class SimpleGenericCache<T>
+    {
+        private readonly Dictionary<string, CachedValue<T>> _cache = new(); 
+        internal void Store(string key, T value, int timeout = 30) 
+        {
+           
+            var cachedValue = new CachedValue<T>
+            {
+                CreationTime = DateTime.Now,
+                Timeout = timeout,
+                Value = value,
+
+            };
+            _cache[key] = cachedValue;
+        }
+
+        internal CachedValue<T>? Fetch(string key)
+        {
+            if (_cache.TryGetValue(key, out var  value)) 
+            {
+                var second = TimeSpan.FromSeconds(value.Timeout);
+                var interval = value.CreationTime + second;
+                if (interval >= DateTime.Now)
+                {
+                    return value;
+                }
+                else
+                {
+                    _cache.Remove(key);
+                }
+            }
+
+            return default; 
+        }
+
+    }
+    public record CachedValue<T>
+    {
+
+        public T? Value { get; init; }
+        public int Timeout { get; init; }
+        public DateTime CreationTime { get; init; }
+
+    }
+}
