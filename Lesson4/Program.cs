5d980367f206ae7b91acea892dd41e06fd33e89d
itate <itate.eg5@gmail.com>
2023-02-26 15:37:37 +0300

    Домашнее задание по лекции 4

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -6,6 +6,6 @@ generic.Store("1","КЭШ");
 
 Console.WriteLine (generic.Fetch("1"));
 
-generic.Store("1", "КЭШ", 0); // Выход в ветку else, возвращает значение по умолчанию (пустоту)
+generic.Store("1", "КЭШ", 0); // Выход в ветку else, возвращает значение по умолчанию (пустоту).
 
 Console.WriteLine(generic.Fetch("1"));
