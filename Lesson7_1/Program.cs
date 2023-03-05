7efa62719884a87b0b8c316b51dd952e3c02c4a2
itate <itate.eg5@gmail.com>
2023-03-05 22:16:30 +0300

    Первая часть домашнего задания по лекции 7

---
+++ Lesson7_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Lesson7_1;
+using static System.Formats.Asn1.AsnWriter;
+
+int M;
+int N;
+
+Console.WriteLine("Введите начальное значение диапозона:");
+var firstNumber = Console.ReadLine() ?? String.Empty;
+
+ while (!int.TryParse(firstNumber, out M) || M < 0)
+{
+    Console.WriteLine("Значение должно быть положительным числом");
+    firstNumber = Console.ReadLine() ?? String.Empty;
+}
+
+Console.WriteLine("Введите последнее значение диапозона:");
+var lastNumber = Console.ReadLine();
+
+while (!int.TryParse(lastNumber, out N) || N < 0)
+{
+    Console.WriteLine("Значение должно быть положительным числом");
+    lastNumber = Console.ReadLine() ?? String.Empty;
+}
+
+while (M > N)
+{
+    Console.WriteLine("Начало диапозона не может быть больше его окончания. Введите последнее значение больше начального");
+    lastNumber = Console.ReadLine();
+    while (!int.TryParse(lastNumber, out N) || N < 0)
+    {
+        Console.WriteLine("Значение должно быть положительным числом");
+        lastNumber = Console.ReadLine() ?? String.Empty;
+    }
+}
+
+_ = new Square_Root(M, N);
---
+++ Lesson7_1/Square_Root.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson7_1
+{
+    internal class Square_Root
+    {
+        public Square_Root (int firstNumber, int lastNumber)
+        {
+            for (int i = firstNumber; i <= lastNumber; i++ )
+            {
+                int number = i;
+                Console.WriteLine ($" Число: {number}, Квадратный корень: {Math.Sqrt(number)}");
+            }
+        }   
+    }
+}
