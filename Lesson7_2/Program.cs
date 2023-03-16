891d2236744d2c0da8840d34cd1c0f3674f11ed7
itate <itate.eg5@gmail.com>
2023-03-16 18:57:27 +0300

    исправлены замечания

--- Lesson7_2/Program.cs
+++ Lesson7_2/Program.cs
@@ -6,16 +6,14 @@ internal class Program
     {
         Console.WriteLine("Введите путь до каталога");
         string path = Console.ReadLine() ?? string.Empty;
-        string newFile = path + @"\result.txt";
-   
+        string newFile = path + @"\resultFile.txt";
          try
         {
-            var allFilePath = Directory.EnumerateFiles(path, "*.txt", SearchOption.AllDirectories); // Находит все текстовые документы
+            var allFilePaths = Directory.EnumerateFiles(path, "*.txt", SearchOption.AllDirectories); // Находит все текстовые документы
 
-            foreach (string txtPath in allFilePath)
+            foreach (string txtPath in allFilePaths)
             {
                 using (StreamReader file = new StreamReader(txtPath)) // Чтение документа
-                {
                     {
                         string text = await file.ReadToEndAsync(); 
 
@@ -25,11 +23,10 @@ internal class Program
                             await mergeFile.WriteAsync(buffer, 0, buffer.Length);
                             Console.WriteLine("Текст записан в файл");
                         }
-                    }
                 }
             }
         }
-        catch (Exception)
+        catch (DirectoryNotFoundException)
         {
             Console.WriteLine("Каталог не найден");
         }
